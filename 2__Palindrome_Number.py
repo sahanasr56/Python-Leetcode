d3f2d2f28b98875697dd31adc4a58ff9ce6f45cf
srsahana <sahana562000@gmail.com>
2026-04-02 22:15:20 +0530

    Palindrome Number

---
+++ 2__Palindrome_Number.py
@@ -0,0 +1,11 @@
+def Palindrome(n):
+    orginal=n
+    rev=0
+    while(n>0):
+        rem=n%10
+        rev=rev*10+rem
+        n=n//10
+    return rev==orginal
+
+result=Palindrome(121)
+print(result)
