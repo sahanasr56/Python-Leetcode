2b20bb9681490e4775f183e36ca8ae87dcafa0ae
srsahana <sahana562000@gmail.com>
2026-04-04 21:30:12 +0530

    Valid Palindrome

---
+++ 4__Valid_Palindrome.py
@@ -0,0 +1,16 @@
+def Palindrome(s):
+    left=0
+    right=len(s)-1
+
+    while left < right:
+        while left<right and not s[left].isalnum():
+            left +=1
+        while left<right and not s[right].isalnum():
+            right -=1
+        if s[left].lower()!=s[right].lower():
+            return False
+        left +=1
+        right -=1
+    return True
+
+print(Palindrome("A man, a plan, a canal: Panama"))
